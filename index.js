6cba15014119b553bb4931aa7efa5179d880ab7a
Muhammad Faisal <digital.1faisal@gmail.com>
2025-10-31 15:29:53 +0500

    minor for vercel

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import app from "./src/app.js";
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
